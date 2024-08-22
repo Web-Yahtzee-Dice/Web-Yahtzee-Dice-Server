81905f972a047fe89169243dd165438adbf2c483
qualificationalitated <hk23g@fsu.edu>
2024-08-23 01:32:37 +0900

    [feat] createGame

--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
@@ -7,9 +7,21 @@ import { UpdateGameDto } from './dto/update-game.dto';
 export class GamesController {
   constructor(private readonly gamesService: GamesService) {}
 
-  @Post()
-  create(@Body() createGameDto: CreateGameDto) {
-    return this.gamesService.create(createGameDto);
+  @Post('create')
+  async createGame(/*@Body() createGameDto: CreateGameDto*/) {
+    try{
+      const game_id = await this.gamesService.createGame(/*createGameDto*/)
+      return {ID : game_id};
+    }catch(error){
+      if(error.message === 'Cannot Make Game'){
+        throw new HttpException(
+          { message:'Cannot Create Game' }, HttpStatus.BAD_REQUEST,
+        );
+      }
+      throw new HttpException(
+        { message:'Internal Server Error' }, HttpStatus.INTERNAL_SERVER_ERROR,
+      )
+    }
   }
 
   @Get()
--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@nestjs/common';
 import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
+import { Game } from './models/game.model';
+import { db } from 'database';
 
 @Injectable()
 export class GamesService {
-  create(createGameDto: CreateGameDto) {
-    return 'This action adds a new game';
+  async createGame(/*createGameDto: CreateGameDto*/) {
+    // 생성할 임의 게임 엔티티를 설정, ID는 데이터베이스에서 만들어주니 속성 제거
+    const newGame: Omit<Game,'game_id'> = {
+      game_winner_id: null,
+      game_status: 'wait',
+      game_start_time: new Date(),
+      game_end_time: null
+    }
+    try{
+      const [game_id] = await db('games')
+        .insert(newGame)
+        
+        return game_id;
+    } catch(error){
+      console.log('Error Create Game:',error);
+      throw new Error('Cannot Make Game')
+    }
   }
 
   findAll() {
