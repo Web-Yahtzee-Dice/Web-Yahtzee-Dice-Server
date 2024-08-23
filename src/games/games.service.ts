12e247e2d6ddfd104b063a1a36558f41dd3f8b54
qualificationalitated <hk23g@fsu.edu>
2024-08-24 02:34:59 +0900

    [fix] game createGame, findAllGame, findGameById api exception works

--- src/games/games.service.ts
+++ src/games/games.service.ts
@@ -3,65 +3,45 @@ import { CreateGameDto } from './dto/create-game.dto';
 import { UpdateGameDto } from './dto/update-game.dto';
 import { Game } from './models/game.model';
 import { db } from 'database';
-import { HttpErrorByCode } from '@nestjs/common/utils/http-error-by-code.util';
 
 @Injectable()
 export class GamesService {
   async createGame(/*createGameDto: CreateGameDto*/) {
     // 생성할 임의 게임 엔티티를 설정, ID는 데이터베이스에서 만들어주니 속성 제거
-    const newGame: Omit<Game,'game_id'> = {
+    const newGame: Omit<Game, 'game_id'> = {
       game_winner_id: null,
       game_status: 'wait',
       game_start_time: new Date(),
       game_end_time: null
     }
-    try{
-      const [game_id] = await db('games')
-        .insert(newGame)
-        return game_id;
-    } catch(error){
-      throw new HttpException(
-        { message:'Internal Server Error' }, 
-        HttpStatus.INTERNAL_SERVER_ERROR,
-      );
-    }
+    const [game_id] = await db('games')
+      .insert(newGame)
+    return game_id;
   }
 
   async findAllGames() {
-    try{
-      const games = await db('games').select('*');
-      return games;
-    } catch(error){
-      throw new HttpException(
-        {message: 'Internal Server Error'},
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
-    }
+
+    const games = await db('games').select('*');
+    return games;
   }
 
   async findGameById(id: string) {
-    try{
-      const numericId = +id;
-      // 숫자가 아닌 id 판별
-      if(isNaN(numericId)){
-        console.error('Invalid Type of ID');
-        throw new BadRequestException('Invalid Type of ID');
-      }
 
-      const game = await db('games').select('*').where('game_id',numericId).first();
-      // ID에 해당하는 게임이 존재하지 않는경우
-      if(!game){
-        console.error(`Game with ID ${numericId} not found`);
-        throw new NotFoundException(`Game with ID ${numericId} not found`);
-      }
-      return game;
-    } catch(error){
-      console.error('Internal Server Error:', error);
-      throw new HttpException(
-        {message:'Internal Server Error'},
-        HttpStatus.INTERNAL_SERVER_ERROR
-      );
+    const numericId = +id;
+    // 숫자가 아닌 id 판별
+    if (isNaN(numericId)) {
+      console.error('Invalid Type of ID');
+      throw new BadRequestException('Invalid Type of ID');
     }
+
+    const game = await db('games').select('*').where('game_id', numericId).first();
+    // ID에 해당하는 게임이 존재하지 않는경우
+    if (!game) {
+      console.error(`Game with ID ${numericId} not found`);
+      throw new NotFoundException(`Game with ID ${numericId} not found`);
+    }
+    return game;
+
   }
 
   updateGame(id: number, updateGameDto: UpdateGameDto) {
