4bc838d06f20da6035a3d9cebf31698cd6554203
qualificationalitated <hk23g@fsu.edu>
2024-09-02 09:30:08 +0900

    [feature] game status, winner update api design

---
+++ src/games/enums/game-status.enum.ts
@@ -0,0 +1,5 @@
+export enum GameStatus{
+    WAIT = 'wait',
+    PROCEED = 'proceed',
+    END = 'end'
+  }
--- src/games/games.controller.ts
+++ src/games/games.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
 import { GamesService } from './games.service';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import path from 'path';
+import { UpdateGameStatusDto } from './dto/update-game-status.dto';
 
 @Controller('games')
 @ApiTags('Games API')
@@ -27,6 +29,21 @@ export class GamesController {
     // 문자열로 받아, + 붙여서 숫자로 변환
     const game = await this.gamesService.findGameById(id);
     return game; 
+  }
+
+  @Patch('update/:id/status')
+  updateGameStatus(@Body() updateGameStatusDto:UpdateGameStatusDto){
+    // 1. 해당 게임이 유저가 플레이하고 있는 게임인지 판단
+    // 2. 해당 게임의 상태가 업데이트 하고자 하는 상태의 이전 상태인지 판단
+    // 3. 게임 진행상태 업데이트
+    // 4. 게임 상태 업데이트 여부 반환
+  }
+
+  @Patch('update/:id/winner')
+  updateGameWinner(){
+    // 1. 해당 게임에 특정 유저가 포함되어 있는지 판단
+    // 2. 해당 게임의 상태가 진행중 - proceed인지 판단
+    // 3. 게임 상태를 end로 업데이트하고 승자 업데이트 후 반환
   }
 
 }
